d04b0408194cd0136e0eb7ca09a7bf2c4bb23b1b
dan-jel <daniel.heese1@gmail.com>
2021-03-08 23:46:02 +0100

    CORS implementation

--- web/app.py
+++ web/app.py
@@ -2,9 +2,11 @@ from flask import Flask, json, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
